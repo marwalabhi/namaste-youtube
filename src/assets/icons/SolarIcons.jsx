11540e59786ba4df0779958a0c12d4d0bb6cbb96
abhimarwal <abhishek.marwal@assessprep.com>
2025-07-04 16:58:10 +0530

    debouncing in search implemented

--- src/assets/icons/SolarIcons.jsx
+++ src/assets/icons/SolarIcons.jsx
@@ -53,3 +53,23 @@ export function SolarMagniferLinear(props) {
     </svg>
   );
 }
+
+export function BitcoinIconsCrossOutline(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      {/* Icon from Bitcoin Icons by Bitcoin Design Community - https://github.com/BitcoinDesign/Bitcoin-Icons/blob/main/LICENSE-MIT */}
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        d="m6 6l12 12m0-12L6 18"
+      />
+    </svg>
+  );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,9 +8,12 @@ import { useDispatch } from "react-redux";
 import { toggleMenu } from "../../utils/slices/appSlice";
 import { useState, useEffect } from "react";
 import { YOUTUBE_SEARCH_API } from "../../utils/constants";
+import { BitcoinIconsCrossOutline } from "../../assets/icons/SolarIcons";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   useEffect(() => {
     //API call
@@ -19,20 +22,47 @@ const Header = () => {
     // make an api call after every key press but if the diff b/w
     // 2 api calls is < 200ms
     // decline the API call
-    getSearchSuggestions();
+
+    const timer = setTimeout(() => {
+      getSearchSuggestions();
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [searchQuery]);
+
+  /**
+   * dry run
+   * - if i press r
+   *
+   * - render the component
+   * - useEffect();
+   * - start timer => make an api call after 200 ms   register a timer in memory of browser of 200ms & js will continue its work
+   *
+   * - even before 200ms i pressed another key stroke
+   * - ra
+   * - it triggers the reconciliation process again, when it triggers reconciliation algo it has to clear things up, cleanup function will be called
+   * - this fn is called when the component is unmounted
+   * - destroy the previous timer
+   * - re-render the component
+   * - useEffect()
+   * - start timer - make api call after 200 ms
+   */
 
   const getSearchSuggestions = async () => {
     const res = await fetch(YOUTUBE_SEARCH_API + searchQuery);
-    const data = res.json();
-    console.log("API responsed with", data);
+    const data = await res.json();
+    console.log(data);
+
+    setSuggestions(data[1] || []);
   };
   const dispatch = useDispatch();
 
   const toggelMenuHandler = () => {
     dispatch(toggleMenu());
   };
-
+  console.log("api call with - ", suggestions);
   return (
     <header className="font-roboto flex items-center justify-between bg-white px-4 py-2 shadow-lg">
       <div className="flex items-center gap-3">
@@ -45,20 +75,49 @@ const Header = () => {
         <img alt="logo" src={logo} className="h-16 w-auto object-contain" />
       </div>
 
-      <form className="mx-6 flex max-w-xl flex-1">
-        <input
-          onChange={(e) => setSearchQuery(e.target.value)}
-          type="text"
-          placeholder="Search"
-          className="flex-1 rounded-l-full border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
-        />
-        <button
-          type="submit"
-          className="rounded-r-full border border-l-0 border-gray-300 bg-gray-100 px-5 py-2 hover:bg-gray-200"
-        >
-          <SolarMagniferLinear />
-        </button>
-      </form>
+      <div className="relative max-w-xl flex-1">
+        <form className="mx-6 flex w-full">
+          <input
+            onChange={(e) => setSearchQuery(e.target.value)}
+            value={searchQuery}
+            type="text"
+            placeholder="Search"
+            className="flex-1 rounded-l-full border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
+            autoComplete="off"
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          {searchQuery && (
+            <button
+              type="button"
+              className="absolute top-1.5 right-10 rounded-full hover:bg-gray-100 focus:outline-none"
+              onClick={() => setSearchQuery("")}
+              tabIndex={-1}
+            >
+              <BitcoinIconsCrossOutline fontSize={32} />
+            </button>
+          )}
+          <button
+            type="submit"
+            className="rounded-r-full border border-l-0 border-gray-300 bg-gray-100 px-5 py-2 hover:bg-gray-200"
+          >
+            <SolarMagniferLinear />
+          </button>
+        </form>
+        {showSuggestions && suggestions.length > 0 && (
+          <ul className="absolute right-0 left-0 z-10 mx-7 mt-1 overflow-y-auto rounded-xl border border-gray-200 bg-white p-2 shadow-lg">
+            {suggestions.map((s, i) => (
+              <li
+                key={s + i}
+                className="flex cursor-pointer items-center gap-2 rounded-xl px-4 py-2 hover:bg-gray-100"
+              >
+                <SolarMagniferLinear className="text-gray-500" />
+                <span className="font-normal">{s}</span>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
 
       <div>
         <button className="cursor-pointer rounded-full p-2 hover:bg-gray-100 focus:outline-none">
